399bacf43d50e761647c6390201a2caac7144c3a
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2023-08-26 14:34:00 -0400

    Added multiple types of RNN models

--- src/main_rnn.py
+++ src/main_rnn.py
@@ -1,10 +1,11 @@
 import os
 from sklearn.utils import shuffle
 from preprocessing import preprocess_text_RNN
-from rnn_model import create_rnn_model
+from rnn_model import create_rnn_model, create_rnn_model_stacked, create_rnn_model_stacked_regularized
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
+from keras.backend import clear_session
 import numpy as np
 
 
@@ -50,18 +51,29 @@ tokenizer.fit_on_texts(train_reviews)
 train_sequences = tokenizer.texts_to_sequences(train_reviews)
 test_sequences = tokenizer.texts_to_sequences(test_reviews)
 
-MAX_LENGTH = 600
+MAX_LENGTH = 1000
 train_padded = pad_sequences(train_sequences, maxlen=MAX_LENGTH, padding='post', truncating='post')
 test_padded = pad_sequences(test_sequences, maxlen=MAX_LENGTH, padding='post', truncating='post')
 
 train_labels = np.array(train_labels)
 test_labels = np.array(test_labels)
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# RNN Model with a single LSTM Layer
+# Define the Learning Rate Schedule
+def lr_schedule(epoch):
+    if epoch < 5:
+        return 0.01
+    else:
+        return 0.001
 
-# Callbacks
-early_stopping = EarlyStopping(monitor='val_loss', patience=3)
-model_checkpoint = ModelCheckpoint('../models/model_weights.h5', monitor='val_loss', save_best_only=True, verbose=1, mode='min')
-callbacks = [early_stopping, model_checkpoint]
+
+# Set up the callbacks
+scheduler = LearningRateScheduler(lr_schedule)
+model_checkpoint = ModelCheckpoint('../models/single_rnn_model_epoch_{epoch:02d}.keras',
+                                   save_best_only=False,
+                                   save_weights_only=False)
+callbacks = [model_checkpoint, scheduler]
 
 # Instantiate and train the model
 model = create_rnn_model(MAX_WORDS, MAX_LENGTH)
@@ -88,5 +100,78 @@ print(f"Test Accuracy: {accuracy:.4f}")
 print(f"Test Precision: {precision:.4f}")
 print(f"Test Recall: {recall:.4f}")
 
+model.save('../models/single_rnn_model.keras')
+clear_session()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# RNN Model with two LSTM layers
 
-model.save('../models/rnn_model.h5')
+# Callbacks
+model_checkpoint = ModelCheckpoint('../models/stacked_rnn_model_epoch_{epoch:02d}.keras',
+                                   save_best_only=False,
+                                   save_weights_only=False)
+callbacks = [model_checkpoint, scheduler]
+
+# Instantiate and train the model
+model = create_rnn_model_stacked(MAX_WORDS, MAX_LENGTH)
+history = model.fit(train_padded, train_labels, epochs=30, batch_size=64, validation_data=(test_padded, test_labels), verbose=1, callbacks=callbacks)
+
+# Parse out the metrics
+train_accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+train_precision = history.history['precision']
+val_precision = history.history['val_precision']
+train_recall = history.history['recall']
+val_recall = history.history['val_recall']
+
+print(f"Training Accuracy: {train_accuracy[-1]:.4f}")
+print(f"Validation Accuracy: {val_accuracy[-1]:.4f}")
+print(f"Training Precision: {train_precision[-1]:.4f}")
+print(f"Validation Precision: {val_precision[-1]:.4f}")
+print(f"Training Recall: {train_recall[-1]:.4f}")
+print(f"Validation Recall: {val_recall[-1]:.4f}")
+
+loss, accuracy, precision, recall = model.evaluate(test_padded, test_labels, verbose=1)
+print(f"Test Loss: {loss:.4f}")
+print(f"Test Accuracy: {accuracy:.4f}")
+print(f"Test Precision: {precision:.4f}")
+print(f"Test Recall: {recall:.4f}")
+
+
+model.save('../models/stacked_rnn_model.keras')
+clear_session()
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# RNN Model with two LSTM layers and regularization
+model_checkpoint = ModelCheckpoint('../models/regularized_rnn_model_epoch_{epoch:02d}.keras',
+                                   save_best_only=False,
+                                   save_weights_only=False)
+callbacks = [model_checkpoint, scheduler]
+
+# Instantiate and train the model
+model = create_rnn_model_stacked_regularized(MAX_WORDS, MAX_LENGTH)
+history = model.fit(train_padded, train_labels, epochs=30, batch_size=64, validation_data=(test_padded, test_labels), verbose=1, callbacks=callbacks)
+
+# Parse out the metrics
+train_accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+train_precision = history.history['precision']
+val_precision = history.history['val_precision']
+train_recall = history.history['recall']
+val_recall = history.history['val_recall']
+
+print(f"Training Accuracy: {train_accuracy[-1]:.4f}")
+print(f"Validation Accuracy: {val_accuracy[-1]:.4f}")
+print(f"Training Precision: {train_precision[-1]:.4f}")
+print(f"Validation Precision: {val_precision[-1]:.4f}")
+print(f"Training Recall: {train_recall[-1]:.4f}")
+print(f"Validation Recall: {val_recall[-1]:.4f}")
+
+loss, accuracy, precision, recall = model.evaluate(test_padded, test_labels, verbose=1)
+print(f"Test Loss: {loss:.4f}")
+print(f"Test Accuracy: {accuracy:.4f}")
+print(f"Test Precision: {precision:.4f}")
+print(f"Test Recall: {recall:.4f}")
+
+
+model.save('../models/regularized_rnn_model.keras')
+clear_session()
--- src/rnn_model.py
+++ src/rnn_model.py
@@ -1,16 +1,43 @@
 from keras import Sequential
 from keras.layers import Embedding, LSTM, Dropout, Dense
-from keras.regularizers import l1
+from keras.regularizers import l2
 from keras.metrics import Precision, Recall
 
 
 def create_rnn_model(MAX_WORDS, MAX_LENGTH):
     model = Sequential([
         Embedding(MAX_WORDS, 128, input_length=MAX_LENGTH),
-        LSTM(128, return_sequences=True),
-        Dropout(0.6),
         LSTM(64),
-        Dropout(0.5),
+        Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
+    return model
+
+
+def create_rnn_model_stacked(MAX_WORDS, MAX_LENGTH):
+    model = Sequential([
+        Embedding(MAX_WORDS, 128, input_length=MAX_LENGTH),
+        LSTM(64, return_sequences=True),
+        LSTM(64),
+        Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
+    return model
+
+
+def create_rnn_model_stacked_regularized(MAX_WORDS, MAX_LENGTH):
+    model = Sequential([
+        Embedding(MAX_WORDS, 128, input_length=MAX_LENGTH),
+        LSTM(64, return_sequences=True,
+             kernel_regularizer=l2(.001),
+             recurrent_regularizer=l2(.001),
+             bias_regularizer=l2(.001)),
+        Dropout(0.2),
+        LSTM(64,
+             kernel_regularizer=l2(.001),
+             recurrent_regularizer=l2(.001),
+             bias_regularizer=l2(.001)),
+        Dropout(0.2),
         Dense(1, activation='sigmoid')
     ])
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
