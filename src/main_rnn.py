2e8927c86f58c50762ba0806a78ecdd6a28d524a
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2023-09-05 15:22:35 -0400

    Restarted LSTM attempts utilizing Keras's sequence of word indexed pre-processed imdb dataset

--- src/main_rnn.py
+++ src/main_rnn.py
@@ -1,87 +1,23 @@
-import os
-from sklearn.utils import shuffle
-from preprocessing import preprocess_text_RNN
-import rnn_model
-from keras.preprocessing.text import Tokenizer
+from keras.datasets import imdb
 from keras.preprocessing.sequence import pad_sequences
-from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
-from keras.backend import clear_session
-import numpy as np
+from keras.callbacks import ModelCheckpoint
+from rnn_model import create_rnn_model
+
+maxwords = 10000
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=maxwords)
+
+maxlen = 500  # Maximum length of the sequences
+x_train = pad_sequences(x_train, maxlen=maxlen, padding='post')
+x_test = pad_sequences(x_test, maxlen=maxlen, padding='post')
 
 
-def load_reviews(path):
-    reviews = []
-    for filename in os.listdir(path):
-        with open(os.path.join(path, filename), 'r', encoding='utf-8') as file:
-            reviews.append(file.read())
-    return reviews
-
-
-# Paths to the data directories
-train_pos_path = '../data/aclImdb/train/pos'
-train_neg_path = '../data/aclImdb/train/neg'
-test_pos_path = '../data/aclImdb/test/pos'
-test_neg_path = '../data/aclImdb/test/neg'
-
-# Load the reviews
-train_pos_reviews = load_reviews(train_pos_path)
-train_neg_reviews = load_reviews(train_neg_path)
-test_pos_reviews = load_reviews(test_pos_path)
-test_neg_reviews = load_reviews(test_neg_path)
-
-# Combine positive and negative reviews and create corresponding labels
-train_reviews = train_pos_reviews + train_neg_reviews
-train_labels = [1] * len(train_pos_reviews) + [0] * len(train_neg_reviews)
-test_reviews = test_pos_reviews + test_neg_reviews
-test_labels = [1] * len(test_pos_reviews) + [0] * len(test_neg_reviews)
-
-# # Shuffle the reviews and labels
-train_reviews, train_labels = shuffle(train_reviews, train_labels)
-test_reviews, test_labels = shuffle(test_reviews, test_labels)
-
-# Preprocess data
-train_reviews = [preprocess_text_RNN(review) for review in train_reviews]
-test_reviews = [preprocess_text_RNN(review) for review in test_reviews]
-
-# Tokenization and Padding
-MAX_WORDS = 60000
-tokenizer = Tokenizer(num_words=MAX_WORDS, oov_token='<UNK>')
-tokenizer.fit_on_texts(train_reviews)
-
-train_sequences = tokenizer.texts_to_sequences(train_reviews)
-test_sequences = tokenizer.texts_to_sequences(test_reviews)
-
-MAX_LENGTH = 1000
-train_padded = pad_sequences(train_sequences, maxlen=MAX_LENGTH, padding='post', truncating='post')
-test_padded = pad_sequences(test_sequences, maxlen=MAX_LENGTH, padding='post', truncating='post')
-
-train_labels = np.array(train_labels)
-test_labels = np.array(test_labels)
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# RNN Model with dropout of 20%
-# Define the Learning Rate Schedule
-def lr_schedule(epoch):
-    if epoch < 5:
-        return 0.05
-    elif epoch < 10:
-        return 0.01
-    else:
-        return 0.001
-
-
-# Set up the callbacks
-scheduler = LearningRateScheduler(lr_schedule)
-model_checkpoint = ModelCheckpoint('../models/rnn_model_do2_epoch_{epoch:02d}.keras',
+model_checkpoint = ModelCheckpoint('../models/model_LSTM_epoch_{epoch:02d}.keras',
                                    save_best_only=False,
                                    save_weights_only=False)
-callbacks = [model_checkpoint, scheduler]
+callbacks = [model_checkpoint]
 
-# Instantiate and train the model
-model = rnn_model.create_rnn_model_stacked_dropout(MAX_WORDS, MAX_LENGTH)
-model.load_weights('../models/stacked_rnn_model_epoch_06.keras')
-history = model.fit(train_padded, train_labels, epochs=30, batch_size=64, validation_data=(test_padded, test_labels), verbose=1, callbacks=callbacks)
+model = create_rnn_model(maxwords, maxlen)
+history = model.fit(x_train, y_train, epochs=10, batch_size=64, validation_data=(x_test, y_test), verbose=1, callbacks=callbacks)
 
 # Parse out the metrics
 train_accuracy = history.history['accuracy']
@@ -98,86 +34,4 @@ print(f"Validation Precision: {val_precision[-1]:.4f}")
 print(f"Training Recall: {train_recall[-1]:.4f}")
 print(f"Validation Recall: {val_recall[-1]:.4f}")
 
-loss, accuracy, precision, recall = model.evaluate(test_padded, test_labels, verbose=1)
-print(f"Test Loss: {loss:.4f}")
-print(f"Test Accuracy: {accuracy:.4f}")
-print(f"Test Precision: {precision:.4f}")
-print(f"Test Recall: {recall:.4f}")
-
-model.save('../models/rnn_model_do2.keras')
-clear_session()
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# RNN Model with dropout of 50%
-
-# Callbacks
-model_checkpoint = ModelCheckpoint('../models/rnn_model_do5_epoch_{epoch:02d}.keras',
-                                   save_best_only=False,
-                                   save_weights_only=False)
-callbacks = [model_checkpoint, scheduler]
-
-# Instantiate and train the model
-model = rnn_model.create_rnn_model_stacked_high_dropout(MAX_WORDS, MAX_LENGTH)
-model.load_weights('../models/stacked_rnn_model_epoch_06.keras')
-history = model.fit(train_padded, train_labels, epochs=30, batch_size=64, validation_data=(test_padded, test_labels), verbose=1, callbacks=callbacks)
-
-# Parse out the metrics
-train_accuracy = history.history['accuracy']
-val_accuracy = history.history['val_accuracy']
-train_precision = history.history['precision']
-val_precision = history.history['val_precision']
-train_recall = history.history['recall']
-val_recall = history.history['val_recall']
-
-print(f"Training Accuracy: {train_accuracy[-1]:.4f}")
-print(f"Validation Accuracy: {val_accuracy[-1]:.4f}")
-print(f"Training Precision: {train_precision[-1]:.4f}")
-print(f"Validation Precision: {val_precision[-1]:.4f}")
-print(f"Training Recall: {train_recall[-1]:.4f}")
-print(f"Validation Recall: {val_recall[-1]:.4f}")
-
-loss, accuracy, precision, recall = model.evaluate(test_padded, test_labels, verbose=1)
-print(f"Test Loss: {loss:.4f}")
-print(f"Test Accuracy: {accuracy:.4f}")
-print(f"Test Precision: {precision:.4f}")
-print(f"Test Recall: {recall:.4f}")
-
-
-model.save('../models/rnn_model_do5.keras')
-clear_session()
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# RNN Model with l1 regularization of .0001
-model_checkpoint = ModelCheckpoint('../models/rnn_model_l1_epoch_{epoch:02d}.keras',
-                                   save_best_only=False,
-                                   save_weights_only=False)
-callbacks = [model_checkpoint, scheduler]
-
-# Instantiate and train the model
-model = rnn_model.create_rnn_model_stacked_regularized(MAX_WORDS, MAX_LENGTH)
-model.load_weights('../models/stacked_rnn_model_epoch_06.keras')
-history = model.fit(train_padded, train_labels, epochs=30, batch_size=64, validation_data=(test_padded, test_labels), verbose=1, callbacks=callbacks)
-
-# Parse out the metrics
-train_accuracy = history.history['accuracy']
-val_accuracy = history.history['val_accuracy']
-train_precision = history.history['precision']
-val_precision = history.history['val_precision']
-train_recall = history.history['recall']
-val_recall = history.history['val_recall']
-
-print(f"Training Accuracy: {train_accuracy[-1]:.4f}")
-print(f"Validation Accuracy: {val_accuracy[-1]:.4f}")
-print(f"Training Precision: {train_precision[-1]:.4f}")
-print(f"Validation Precision: {val_precision[-1]:.4f}")
-print(f"Training Recall: {train_recall[-1]:.4f}")
-print(f"Validation Recall: {val_recall[-1]:.4f}")
-
-loss, accuracy, precision, recall = model.evaluate(test_padded, test_labels, verbose=1)
-print(f"Test Loss: {loss:.4f}")
-print(f"Test Accuracy: {accuracy:.4f}")
-print(f"Test Precision: {precision:.4f}")
-print(f"Test Recall: {recall:.4f}")
-
-
-model.save('../models/rnn_model_l1.keras')
-clear_session()
+model.save('../models/model_LSTM.keras')
--- src/rnn_model.py
+++ src/rnn_model.py
@@ -1,67 +1,12 @@
 from keras import Sequential
 from keras.layers import Embedding, LSTM, Dropout, Dense
-from keras.regularizers import l1, l2
 from keras.metrics import Precision, Recall
 
 
 def create_rnn_model(MAX_WORDS, MAX_LENGTH):
     model = Sequential([
-        Embedding(MAX_WORDS, 128, input_length=MAX_LENGTH),
+        Embedding(MAX_WORDS, 50, input_length=MAX_LENGTH),
         LSTM(64),
-        Dense(1, activation='sigmoid')
-    ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
-    return model
-
-
-def create_rnn_model_stacked(MAX_WORDS, MAX_LENGTH):
-    model = Sequential([
-        Embedding(MAX_WORDS, 128, input_length=MAX_LENGTH),
-        LSTM(64, return_sequences=True),
-        LSTM(64),
-        Dense(1, activation='sigmoid')
-    ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
-    return model
-
-
-def create_rnn_model_stacked_regularized(MAX_WORDS, MAX_LENGTH):
-    model = Sequential([
-        Embedding(MAX_WORDS, 128, input_length=MAX_LENGTH),
-        LSTM(64, return_sequences=True,
-             kernel_regularizer=l1(.0001),
-             recurrent_regularizer=l1(.0001),
-             bias_regularizer=l1(.0001)),
-        LSTM(64,
-             kernel_regularizer=l1(.0001),
-             recurrent_regularizer=l1(.0001),
-             bias_regularizer=l1(.0001)),
-        Dense(1, activation='sigmoid')
-    ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
-    return model
-
-
-def create_rnn_model_stacked_dropout(MAX_WORDS, MAX_LENGTH):
-    model = Sequential([
-        Embedding(MAX_WORDS, 128, input_length=MAX_LENGTH),
-        LSTM(64, return_sequences=True),
-        Dropout(0.2),
-        LSTM(64),
-        Dropout(0.2),
-        Dense(1, activation='sigmoid')
-    ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
-    return model
-
-
-def create_rnn_model_stacked_high_dropout(MAX_WORDS, MAX_LENGTH):
-    model = Sequential([
-        Embedding(MAX_WORDS, 128, input_length=MAX_LENGTH),
-        LSTM(64, return_sequences=True),
-        Dropout(0.5),
-        LSTM(64),
-        Dropout(0.5),
         Dense(1, activation='sigmoid')
     ])
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
